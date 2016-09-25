a33017b7a1e0701c48e6195cf8bfaec226e210bf
Emiliakazoo <emiliakazoo@gmail.com>
2016-09-25 09:35:50 -0700

    Hopefully fixed

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
 
 $(document).ready(function(){
-	var x = 100;
-	for (var i = 1; i <= x; i++) {
+
+var usrNbr;
+$("#fizz-buzz-entry-form").on("submit",function(event){
+		event.preventDefault();     
+    	var usrInput = $("#fizz-buzz-entry").val();
+
+		if(isNaN(usrInput)) {
+			alert("Please enter a REAL number, dawg.");
+			$("#fizz-buzz-entry").val("").focus();
+			return;
+		} else if (usrInput % 1 != 0){
+			alert("A WHOLE number, please.  No decimals.");
+			$("#fizz-buzz-entry").val("").focus();
+		} else{
+			usrNbr = +usrInput;    
+	    	$("#fizz-buzz-entry").val("");	
+	    	fizzBuzz(usrNbr);
+		}
+
+
+});
+
+function fizzBuzz(usrNbr){
+
+	for (var i = 1; i <= usrNbr; i++) {
 		if((i % 3 == 0) && (i % 5 == 0)){
 			$("#fizzBuzzUL").append("<li>Fizz Buzz");
 		}
 		else if(i % 3 == 0) {
 			$("#fizzBuzzUL").append("<li>Fizz</li>");
+		    //console.log("Fizz");
 			}
 		else if(i % 5 == 0) {
 			$("#fizzBuzzUL").append("<li>Buzz</li>");
+		    //console.log("Buzz");
 		  }
 		else {
 			$("#fizzBuzzUL").append("<li>" + i + "</li>");
+			//console.log(i);
 		}
 	}
+
+}
 
 });
 
